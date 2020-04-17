7e5bd8ce1e76351b4005bcf8f9bcc7f6fb43a4c7
Daniel Newman <danielnewman09@gmail.com>
2020-04-17 10:47:44 -0400

    updates

---
+++ Dissertation-Notebooks/Chapter-4/data/Latency Tests/perform_experiments.py
@@ -0,0 +1,78 @@
+import numpy as np
+from run_models import RunModels
+from time import process_time 
+import pandas as pd
+
+
+modelIds = ['PCA-GMM','AE','CNN-AE','CNN-AE-Lite','PCA-GNB','MLP','CNN-MLP','CNN-MLP-Lite']
+data_labels = ['Train_Healthy','Valid_Healthy','Train_Unhealthy','Valid_Unhealthy']
+
+INFERENCE_LOCATION = 'Desktop'
+MODEL_STATUS = 'Unloaded'
+BASE_PATH = '/home/dnewman/'
+
+
+if INFERENCE_LOCATION == 'Desktop':
+        import tensorflow as tf
+        gpus= tf.config.experimental.list_physical_devices('GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
+if MODEL_STATUS == 'Loaded':
+    preload_models = True
+else:
+    preload_models = False
+
+thisModel = RunModels(preload_models,BASE_PATH,desktop=True)
+
+for k in range(4):
+
+    testData = np.genfromtxt(data_labels[k] + '.csv',delimiter=',')
+    latency = np.zeros((testData.shape[0],len(modelIds)))
+    inferenceVals = np.zeros((testData.shape[0],len(modelIds)))
+
+        
+    for i in range(testData.shape[0]):
+
+        values = testData[i,:].flatten()
+
+        if i % 10 == 0:
+            print('Processing Sample {}'.format(i))
+
+        for j in range(len(modelIds)):
+            modelId = modelIds[j]
+
+            start_time = process_time()
+
+            if j == 0:
+                value = thisModel.model_gmm(values,modelId)
+            elif j == 1 or  j == 2:
+                value = thisModel.model_inference_full(values,modelId)
+            elif j == 3:
+                value = thisModel.model_inference_lite(values,modelId)
+            elif j == 4:   
+                value = thisModel.model_gnb(values,modelId) 
+            elif j == 5 or j == 6:
+                value = thisModel.classifier_inference_full(values,modelId)
+            elif j == 7:
+                value = thisModel.classifier_inference_lite(values,modelId)
+
+            end_time = process_time()
+
+            thisLatency = end_time - start_time
+
+            latency[i,j] = thisLatency
+            inferenceVals[i,j] = value
+
+
+    myDF = pd.DataFrame(data=latency,columns=modelIds)
+    myDF.to_csv('Results/' + INFERENCE_LOCATION + '_' + MODEL_STATUS + '_' + data_labels[k] + '.csv')
+
+    myDF = pd.DataFrame(data=inferenceVals,columns=modelIds)
+    myDF.to_csv('Results/' + INFERENCE_LOCATION + '_' + MODEL_STATUS + '_' + data_labels[k] +  '_values.csv')
+
+
+
+            
+
+            
---
+++ Dissertation-Notebooks/Chapter-4/data/Latency Tests/run_models.py
@@ -0,0 +1,193 @@
+#!flask/bin/python
+
+import os
+import numpy as np
+
+from scipy import signal
+from scipy.stats import describe
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.metrics import mean_squared_error
+import tflite_runtime.interpreter as tflite
+# import tensorflow as tf
+# import tensorflow.keras as keras
+# from Custom_Layers import Dropout_Live
+
+from joblib import dump, load
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))    
+
+class RunModels(object):
+    def __init__(self,preload_models,basePath,desktop=False):
+
+        self.basePath = basePath
+        self.preload_models = preload_models
+
+        if preload_models == True:
+
+            self.pca_gmm_model = load(basePath + "Models/GMM/PCA-GMM.joblib")
+            self.cnn_ae_model = load_model(basePath + "Models/Autoencoder/Full/CNN-AE.h5")
+            self.ae_model = load_model(basePath + "Models/Autoencoder/Full/AE.h5")
+            self.cnn_ae_lite_model = tflite.Interpreter(model_path=self.basePath + "Models/Autoencoder/Lite/CNN-AE-Lite.tflite")
+
+            self.pca_gnb_model = load(basePath + "Models/GNB/PCA-GNB.joblib")
+            self.mlp_model = load_model(basePath + "Models/MLP-Classifier/Full/MLP.h5")
+            self.cnn_mlp_model = load_model(basePath + "Models/MLP-Classifier/Full/CNN-MLP.h5")
+            self.cnn_mlp_lite_model = tflite.Interpreter(model_path=self.basePath + "Models/MLP-Classifier/Lite/CNN-MLP-Lite.tflite")
+
+        
+
+
+    def classifier_inference_full(self,values,modelId):
+
+        xInference = np.atleast_2d(np.array(values).astype(np.float32))
+
+        if self.preload_models:
+
+            if 'cnn' in modelId.lower():
+                model = self.cnn_mlp_model
+            else:
+                model = self.mlp_model
+        else:
+            if 'cnn' in modelId.lower():
+                model = load_model(self.basePath + "Models/MLP-Classifier/Full/CNN-MLP.h5")
+            else:
+                model = load_model(self.basePath + "Models/MLP-Classifier/Full/MLP.h5")
+
+        X_predict = np.atleast_2d(xInference)
+
+        if 'cnn' in modelId.lower():
+            X_predict = X_predict[...,np.newaxis]
+
+        predict = model.predict(X_predict)
+        classification = predict[0,0].astype(float)
+
+        return classification
+
+    def model_inference_full(self,values,modelId):
+
+        xInference = np.atleast_2d(np.array(values).astype(np.float32))
+
+        if self.preload_models == True:
+
+            if 'cnn' in modelId.lower():
+                model = self.cnn_ae_model
+            else:
+                model = self.ae_model
+        else:
+            if 'cnn' in modelId.lower():
+                model = load_model(self.basePath + "Models/Autoencoder/Full/CNN-AE.h5")
+            else:
+                model = load_model(self.basePath + "Models/Autoencoder/Full/AE.h5")
+
+        X_predict = np.atleast_2d(xInference)
+
+        if 'cnn' in modelId.lower():
+            X_predict = X_predict[...,np.newaxis]
+
+        predict = model.predict(X_predict)
+        mse = mean_squared_error(X_predict,predict).numpy().flatten()[0].astype(float)
+
+        return mse
+
+    def model_inference_lite(self,values,modelId):
+
+        xInference = np.atleast_2d(np.array(values).astype(np.float32))
+
+        if self.preload_models == True:
+            interpreter = self.cnn_ae_lite_model
+        else:
+            interpreter = tflite.Interpreter(model_path=self.basePath + "Models/Autoencoder/Lite/CNN-AE-Lite.tflite")
+
+        interpreter.allocate_tensors()
+
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        # Test model on random input data.
+        input_shape = input_details[0]['shape']
+        # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+        input_data = xInference.reshape(input_shape).astype(np.float32)
+
+        num_samples = 1
+
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+
+        # The function `get_tensor()` returns a copy of the tensor data.
+        # Use `tensor()` in order to get a pointer to the tensor.
+        output_data = interpreter.get_tensor(output_details[0]['index']).reshape(input_shape)
+
+        input_data = np.repeat(input_data,num_samples,axis=0)
+
+        mse = mean_squared_error(output_data,input_data).numpy().flatten()[0].astype(float)
+
+        return mse
+
+
+    def classifier_inference_lite(self,values,modelId):
+
+        xInference = np.atleast_2d(np.array(values).astype(np.float32))
+
+
+        if self.preload_models:
+            interpreter = self.cnn_mlp_lite_model
+        else:
+            interpreter = tflite.Interpreter(model_path=self.basePath + "Models/MLP-Classifier/Lite/CNN-MLP-Lite.tflite")
+
+        interpreter.allocate_tensors()
+
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        
+        # Test model on random input data.
+        input_shape = input_details[0]['shape']
+        # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+        input_data = xInference.reshape(input_shape).astype(np.float32)
+        output_shape = output_details[0]['shape']
+
+
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+
+        # The function `get_tensor()` returns a copy of the tensor data.
+        # Use `tensor()` in order to get a pointer to the tensor.
+        output_data = interpreter.get_tensor(output_details[0]['index']).flatten()
+        
+        classification = output_data[0].astype(float)
+
+        return classification
+
+    def model_gmm(self,values,modelId):
+
+        xInference = np.atleast_2d(values).astype(np.float32)
+
+        if self.preload_models:
+            model = self.pca_gmm_model
+        else:
+            model = load(self.basePath + "Models/GMM/PCA-GMM.joblib")
+
+        log_likelihood = model.score_samples(xInference)
+
+        return log_likelihood.flatten()[0].astype(float)
+
+    def model_gnb(self,values,modelId):
+
+        xInference = np.atleast_2d(np.array(values).astype(np.float32))
+
+        if self.preload_models:
+            model = self.pca_gnb_model
+        else:
+            model = load(self.basePath + "Models/GNB/PCA-GNB.joblib")
+
+        classification = model.predict_proba(xInference).flatten()[0]
+
+        return classification
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
+
